e44f5c14cc98e565309bd29e0b027d3962a56008
DanielZsirko <zsirko.daniel.attila@blathy.info>
2024-10-18 11:30:24 +0200

    Create Console App and "hajo" class

---
+++ Csharp/ConsoleApp1/ConsoleApp1/hajo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class hajo
+    {
+        private string name;
+        private int speed;
+        private int year;
+        private string id;
+
+        public hajo(string name, int speed, int year, string id)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.year = year;
+            this.id = id;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Speed { get => speed; set => speed = value; }
+        public int Year { get => year; set => year = value; }
+        public string Id { get => id; set => id = value; }
+
+        public override string? ToString()
+        {
+            return $"A hajó neve: {name}, sebessége: {speed}, évjárata: {year}, azonosítója: {id}";
+        }
+    }
+}
